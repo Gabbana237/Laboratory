f605c34e5b72e640f3d3491230ee92b62c692ce3
Gabbana237 <mefirehamed936@gmail.com>
2024-12-21 08:22:22 -0800

    second commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+    <h1 className="text-3xl font-bold text-blue-500">Bienvenue sur React + Tailwind !</h1>
+  </div>
   );
 }
 
