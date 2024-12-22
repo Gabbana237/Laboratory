e817b8c14e876e03217b42fa8a8061ca394f63fb
Spinoza235 <esperancedingammadji785@gmail.com>
2024-12-22 04:42:07 +0100

    modif footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue: '#314d66',
+        turquoiseLight: '#1eb798',
+        emeraldLight: '#3bc0a5',
+        lightGray: '#e2eceb',
+        blueGrayDark: '#384555',
+        brightBlue: '#2d7fb6',
+        turquoiseDark: '#1da389',
+        blueGray: '#5288ab',
+        graym: '#ccd0d4',
+        darkGreen: '#299581',
+      },
+    },
   },
   plugins: [],
 };
